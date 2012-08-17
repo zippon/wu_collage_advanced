186ab49d21423e07d40b8ee8e1f7c64e0fc2f3f3
zippon <zippon.wu@gmail.com>
2012-08-17 12:13:26 +0900

    Initial Commit

---
+++ wu_collage_advanced/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  wu_collage_advanced
+//
+//  Created by Zhipeng Wu on 8/17/12.
+//  Copyright (c) 2012 Zhipeng Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
